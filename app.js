d8b7c98291624f9a1b8c050021703efb027a58ce
Markus Finell <markus@thegeneration.se>
2020-11-14 15:45:05 +0100

    Add arrowkey controls

--- app.js
+++ app.js
@@ -9,103 +9,129 @@ const gridSize = 10;
 const gridWidth = Math.round(boardWidth / gridSize);
 const gridHeight = Math.round(boardHeight / gridSize);
 const gridArea = gridWidth * gridHeight;
+const grid = [];
+for ( let x = 0; x < gridWidth; x++ ) {
+	for ( let y = 0; y < gridHeight; y++ ) {
+		grid.push( x + '-' + y );
+	}
+}
 
 const controls = document.querySelectorAll( '#controls button' );
 
-let snake = [ [0,0], [0,1] ];
+let snakeHead = [0,0];
+let snake = () => document.getElementsByClassName( 'snake' );
 let snakeDirection = [0,1];
 let moveInterval;
 
 let treat = [-1,-1];
-const treatEl = document.createElement( 'span' );
+const treatEl = document.createElement( 'div' );
+treatEl.classList.add( 'treat' );
+treatEl.style.width = gridSize + 'px';
+treatEl.style.height = gridSize + 'px';
+treatEl.id = 't--1--1';
+board.appendChild( treatEl );
 
 let points = 0;
+let newPoint = false;
 const pointsEl = document.getElementById( 'points' );
 
-controls.forEach( el => {
-	el.addEventListener('touchstart', (e) => {
-		let moveY = +e.target.getAttribute('data-y') || 0;
-		let moveX = +e.target.getAttribute('data-x') || 0;
-		if ( snakeDirection[0] != 0 && moveY != 0 ) {
-			return;
-		}
-		if ( snakeDirection[1] != 0 && moveX != 0 ) {
-			return;
-		}
-		snakeDirection[0] = moveY;
-		snakeDirection[1] = moveX;
-		moveSnake();
-		play();
-	} );
-} );
+let playing = false;
+let moveCount = 0;
+
+const controlKeys = {
+	ArrowUp: { x: 0, y: -1 },
+	ArrowDown: { x: 0, y: 1 },
+	ArrowLeft: { x: -1, y: 0 },
+	ArrowRight: { x: 1, y: 0 }
+};
+
+resetInterval = () => {
+	clearInterval( moveInterval );
+    moveInterval = setInterval( moveSnake, 200 );
+};
+
+turn = ( x, y ) => {
+	if ( snakeDirection[0] != 0 && y != 0 ) {
+		return;
+	}
+	
+	if ( snakeDirection[1] != 0 && x != 0 ) {
+		return;
+	}
+
+	snakeDirection[0] = y;
+	snakeDirection[1] = x;
+	
+	resetInterval();
+	moveSnake();
+	
+};
 
 drawSnake = () => {
-	for ( let i = 0; i < snake.length; i++ ) {
-		let bit = document.createElement( 'div' );
-		bit.setAttribute( 'id', 's-' + i );
-		bit.style.width = gridSize + 'px';
-		bit.style.height = gridSize + 'px';
-		bit.style.top = gridSize * snake[i][0] + 'px';
-		bit.style.left = gridSize * snake[i][1] + 'px';
-		board.appendChild(bit);
+	if ( snake().length && ! newPoint ) {
+		snake()[0].remove();
 	}
+	newPoint = false;
+	addSnakeBit();
 };
-drawSnake();
+
+addSnakeBit = () => {
+	let bit = document.createElement( 'div' );
+	bit.classList.add( 'snake' );
+	bit.id = 's-' + snakeHead[0] + '-' + snakeHead[1];
+	bit.style.width = gridSize + 'px';
+	bit.style.height = gridSize + 'px';
+	bit.style.top = gridSize * snakeHead[0] + 'px';
+	bit.style.left = gridSize * snakeHead[1] + 'px';
+	board.appendChild( bit );
+};
 
 drawTreat = () => {
 	treatEl.style.top = gridSize * treat[0] + 'px';
-    treatEl.style.left = gridSize * treat[1] + 'px';
-   	board.appendChild(treatEl);
+	treatEl.style.left = gridSize * treat[1] + 'px';
 };
-drawTreat();
 
 setPoints = () => {
 	pointsEl.innerHTML = points;
 };
 
-snakeHead = ( i ) => {
-	return snake[snake.length-1][i];
-};
-
 moveTreat = () => {
-	let grid = [];
-    for ( let i = 0; i < gridArea; i++ ) {
-		grid.push(i);
-	}
-	let currentSnake = snake.map( el => {
-		return el[0] * gridWidth + el[1];
+	let currentGrid = grid.filter( el => {
+		return document.getElementById( 's-' + el ) == null;
 	} );
-	let currentGrid = grid.filter(el => {
-		return currentSnake.indexOf(el) == -1;
-	} );
+
 	let gridKey = Math.round( Math.random() * currentGrid.length - 1 );
-	let cellNum = currentGrid[ gridKey ] / gridWidth;
-	let row = Math.floor( cellNum );
-	let col = Math.round( ( cellNum - row ) * gridWidth );
-	treat = [row, col];
+	let cell = currentGrid[ gridKey ].split( '-' );
+	treat = [cell[0], cell[1]];
+	treatEl.id = 't-' + cell[0] + '-' + cell[1];
+
+	drawTreat();
 };
-moveTreat();
 
 collisionDetected = () => {
-	if ( snakeHead(0) < 0 || snakeHead(1) < 0 ) {
+	if ( snakeHead[0] < 0 || snakeHead[1] < 0 ) {
 		return true;
 	}
-	if ( snakeHead(0) > boardHeight / gridSize - 1 ) {
+
+	if ( snakeHead[0] > boardHeight / gridSize - 1 ) {
 		return true;
 	}
-	if ( snakeHead(1) > boardWidth / gridSize - 1 ) {
+	
+	if ( snakeHead[1] > boardWidth / gridSize - 1 ) {
 		return true;
 	}
-	for ( let i = 0; i < snake.length - 1; i++ ) {
-		if ( snakeHead(0) == snake[i][0] && snakeHead(1) == snake[i][1] ) {
-			return true;
-		}
+
+	if ( document.getElementById( 's-' + snakeHead[0] + '-' + snakeHead[1] ) ) {
+		return true;
 	}
-	if ( snakeHead(0) == treat[0] && snakeHead(1) == treat[1] ) {
+
+	if ( treatEl.id == 't-' + snakeHead[0] + '-' + snakeHead[1] ) {
 		points++;
-		snake.splice( 0, 0, snake[0] );
+		newPoint = true;
+		setPoints();
 		moveTreat();
 	}
+
 	return false;
 };
 
@@ -113,7 +139,12 @@ gameOver = () => {
 	clearInterval( moveInterval );
 	if ( confirm( 'You got ' + points + ' points! Play again?' ) ) {
 		points = 0;
-		snake = [ [0,0], [0,1] ];
+		snakeHead = [0,0];
+
+		while ( snake().length ) {
+			snake()[0].remove();
+		}
+
 		moveTreat();
 		snakeDirection = [0,1];
 		drawSnake();
@@ -122,22 +153,38 @@ gameOver = () => {
 };
 
 moveSnake = () => {
-	snake.splice(0,1);
-	moveY = snake[snake.length-1][0] + snakeDirection[0];
-	moveX = snake[snake.length-1][1] + snakeDirection[1];
-	snake.push([moveY, moveX]);
+	snakeHead[0] += snakeDirection[0];
+	snakeHead[1] += snakeDirection[1];
+	
 	if ( collisionDetected() ) {
 		gameOver();
 	} else {
 		drawSnake();
-	} 
+	}
 };
 
 play = () => {
-	clearInterval(moveInterval);
-    moveInterval = setInterval(moveSnake, 200);
+	drawSnake();
+	moveTreat();
+	setPoints();
+	resetInterval();
 };
 
-if(confirm('ready?')){
-	play();
-}
+controls.forEach( el => {
+	el.addEventListener( 'click', (e) => {
+		let moveY = +e.target.getAttribute('data-y') || 0;
+		let moveX = +e.target.getAttribute('data-x') || 0;
+		
+		turn( moveX, moveY );
+	} );
+} );
+
+document.querySelector( 'body' ).addEventListener( 'keydown', e => {
+	turn( controlKeys[e.key].x, controlKeys[e.key].y );
+} );
+
+play();
+
+// if ( confirm( 'ready?' ) ) {
+// 	play();
+// }
